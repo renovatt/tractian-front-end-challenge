9a359907392837f45277e31fe30a35c0c302cf9c
will <wlymes@gmail.com>
2024-08-10 11:38:27 -0300

    refactor: search loading

--- src/components/features/tree-node/_components/tree-node-assets.tsx
+++ src/components/features/tree-node/_components/tree-node-assets.tsx
@@ -6,23 +6,33 @@ import { useTreeNodeData } from '@/hooks/use-tree-node-data'
 import { useSearchStore } from '@/stores/use-search-store'
 
 export default function TreeNodeAssets() {
+  const [isSearchLoading, setIsSearchLoading] = useState(false)
   const [isFilteredTree, setIsFilteredTree] = useState<TreeNode[]>([])
   const { loading, treeData, searchTree } = useTreeNodeData()
   const search = useSearchStore((state) => state.state.search)
 
   useEffect(() => {
-    if (search) {
-      const newFilteredTree = searchTree(treeData, search)
-      setIsFilteredTree(newFilteredTree)
-    } else {
-      setIsFilteredTree(treeData)
+    const performSearch = async () => {
+      if (search) {
+        setIsSearchLoading(true)
+        await new Promise((resolve) => setTimeout(resolve, 500))
+        const newFilteredTree = searchTree(treeData, search)
+        setIsFilteredTree(newFilteredTree)
+        setIsSearchLoading(false)
+      } else {
+        setIsFilteredTree(treeData)
+      }
     }
+
+    performSearch()
   }, [search, treeData, searchTree])
 
   return (
     <>
-      {loading ? (
-        <p>Loading...</p>
+      {loading || isSearchLoading ? (
+        <p>Buscando...</p>
+      ) : !isFilteredTree.length ? (
+        <p>Nenhum dado foi encontrado</p>
       ) : (
         isFilteredTree.map((node) => (
           <TreeNodeComponent key={node.id} node={node} />
